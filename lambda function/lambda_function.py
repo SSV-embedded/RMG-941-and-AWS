5644ed1a37a0f9f51643d8d1333f7936ac8b13ec
Franz Bu <49677660+fbussv@users.noreply.github.com>
2020-04-21 11:38:47 +0200

    Update lambda_function.py

--- lambda function/lambda_function.py
+++ lambda function/lambda_function.py
@@ -3,7 +3,7 @@ import boto3
 import numpy as np
 import io
 
-# endpoint name defined in Jupyter notebook whe the endpoint is created.
+# endpoint name defined in Jupyter notebook when the endpoint is created.
 # You will need it to call the endpoint to do a new prediction.
 endpoint_name = "<your endpoint>"
 
@@ -29,13 +29,13 @@ def lambda_handler(event, context):
     # Send the prediction result back to our field sof sensor. 
     iot = boto3.client('iot-data', region_name='eu-central-1')
 
-    # This topic defined here must be the same as the topic in Node-RED:
+    # Topic defined here must be the same as the subscribed topic in Node-RED:
     response = iot.publish(topic='status/mls160/', qos=1, payload=str(y_class))
     
     # As an example: If class is 2, then send message to mobile phone. Be sure to have the policy permission set. 
     if(y_class == 2): 
         
         # AWS SNS client
-        sns = boto3.client('sns', region_name='eu-west-1')
+        sns = boto3.client('sns', region_name='eu-west-1') # SNS dont support every region
         number = "<cellphone numer>"
         sns.publish(PhoneNumber = number, Message="Hello this is a message!")
